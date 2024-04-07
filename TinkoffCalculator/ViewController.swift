adddb6be5bd469aee81c227dd1e2f1e5c4e25433
AlexandraLeonova <alexleonova2013@gmail.com>
2024-04-07 16:54:33 +0300

    add calculator logic

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  TinkoffCalculator
 //
@@ -7,18 +6,133 @@
 
 import UIKit
 
+enum CalculationError: Error {
+    case dividedByZero
+}
+
+enum Operation: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "X"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+        case .add:
+            return number1 + number2
+        case .substract:
+            return number1 - number2
+        case .multiply:
+            return number1 * number2
+        case .divide:
+            if number2 == 0 {
+                throw CalculationError.dividedByZero
+            }
+            return number1 / number2
+        }
+    }
+}
+
+enum CalculationHistotyItem {
+    case number(Double)
+    case operation(Operation)
+}
+
 class ViewController: UIViewController {
     
-    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        print(sender.title(for: .normal)!)
+        guard let buttonText = sender.currentTitle else { return }
+        
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if label.text == "0" || label.text == "Ошибка" {
+            label.text = buttonText
+        } else {
+            label.text?.append(buttonText)
+        }
     }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle,
+        let buttonOperation = Operation(rawValue: buttonText) else { return }
+        
+        guard let labelText = label.text,
+              let labelNumber = numberFormatter.number(from: labelText)?.doubleValue else { return }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        
+        resetLabelText()
+    }
+    
+    @IBAction func calculateButtonPressed() {
+        guard
+            let labelText = label.text,
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+        else {
+            return
+        }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            label.text = numberFormatter.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Ошибка"
+        }
+        
+        calculationHistory.removeAll()
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var calculationHistory: [CalculationHistotyItem] = []
+    
+    let numberFormatter: NumberFormatter = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = Locale(identifier: "ru_RU")
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        resetLabelText()
+    }
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            
+            guard
+                case .operation(let operation) = calculationHistory[index],
+                case .number(let number) = calculationHistory[index + 1]
+            else {
+                break
+            }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
+    }
+    
+    func resetLabelText() {
+        label.text = "0"
     }
 
-
 }
-
